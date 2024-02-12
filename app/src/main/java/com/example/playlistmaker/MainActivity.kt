578e762b9460b8474902ae800b39122ecd775f8c
Corpseofpig <Corpseofpig@mail.ru>
2024-02-12 13:26:07 +0300

    Выполнение домашних заданий

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search_button = findViewById<Button>(R.id.search_button)
+        search_button.setOnClickListener{
+            val searchIntent = Intent (this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        val library_button = findViewById<Button>(R.id.library_button)
+        library_button.setOnClickListener{
+            val libraryIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+
+        val settings_button = findViewById<Button>(R.id.settings_button)
+        settings_button.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
+
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val buttonBack = findViewById<Button>(R.id.button_back)
+        buttonBack.setOnClickListener{
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+    }
+}
